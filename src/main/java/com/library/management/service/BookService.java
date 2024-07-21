9a49cd19b3dad14b5fa1424b54e24c3492d0ef2d
Muhammed ALAGOZ <muhammed.alagoz@consultant.turkcell.com.tr>
2024-07-21 15:42:43 +0300

    Add log

--- src/main/java/com/library/management/service/BookService.java
+++ src/main/java/com/library/management/service/BookService.java
@@ -6,6 +6,8 @@ import com.library.management.mapper.BookMapper;
 import com.library.management.model.Book;
 import com.library.management.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,31 +17,34 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class BookService {
+    private static final Logger logger = LoggerFactory.getLogger(BookService.class);
 
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
 
     public List<BookDetailResponse> getAllBooks() {
+        logger.info("Fetching all books");
         return bookRepository.findAll().stream()
                 .map(bookMapper::toDetailResponse)
                 .toList();
     }
 
     public Optional<BookDetailResponse> getBookById(Long id) {
+        logger.info("Fetching book by ID: {}", id);
         return bookRepository.findById(id).map(bookMapper::toDetailResponse);
     }
 
     @Transactional
     public BookDetailResponse addBook(CreateBookRequest request) {
+        logger.info("Adding a new book with request: {}", request);
         final var book = bookMapper.toBook(request);
-
         var persistedBook = bookRepository.save(book);
-
         return bookMapper.toDetailResponse(persistedBook);
     }
 
     @Transactional
     public Optional<BookDetailResponse> updateBook(Long id, Book bookDetails) {
+        logger.info("Updating book with ID: {}", id);
         Book book = bookRepository.findByIdWithLock(id);
         if (book != null) {
             book.setTitle(bookDetails.getTitle());
@@ -57,16 +62,19 @@ public class BookService {
 
     @Transactional
     public void deleteBook(Long id) {
+        logger.info("Deleting book with ID: {}", id);
         bookRepository.deleteById(id);
     }
 
     public List<BookDetailResponse> getBooksByGenre(String genre) {
+        logger.info("Fetching books by genre: {}", genre);
         return bookRepository.findByGenre(genre).stream()
                 .map(bookMapper::toDetailResponse)
                 .toList();
     }
 
     public List<BookDetailResponse> getBooksByAuthor(String author) {
+        logger.info("Fetching books by author: {}", author);
         return bookRepository.findByAuthorContainingIgnoreCase(author).stream()
                 .map(bookMapper::toDetailResponse)
                 .toList();
